45d36fd5b1502ce7202473f36d4f7f72c04cbb9c
Your Name <your_email@real_email.edu>
2020-06-18 16:34:07 -0400

    worked on second day

--- project_16/src/components/index.js
+++ project_16/src/components/index.js
@@ -1,1 +1,3 @@
 export { default as Header } from './Header';
+export { default as UserTodos } from './UserTodos';
+export { default as UserPosts } from './UserPosts';
--- project_16/src/index.js
+++ project_16/src/index.js
@@ -1,39 +1,73 @@
-import React, { useState, useEffect } from 'react';
-import ReactDOM from 'react-dom';
+import React, { useState, useEffect } from "react";
+import ReactDOM from "react-dom";
+
+import { Header, UserPosts, UserTodos } from "./components/index.js";
 
 import {
-  Header
-} from './components';
-
-import {
-    getUsers
-  } from './api/index.js';
-  
+  getUsers,
+  getPostsByUser,
+  getTodosByUser, // NEW
+} from "./api/index.js";
 
 const App = () => {
   const [userList, setUserList] = useState([]);
   const [currentUser, setCurrentUser] = useState(null);
+  const [userPosts, setUserPosts] = useState([]);
+  const [userTodos, setUserTodos] = useState([]); // NEW
 
   useEffect(() => {
     getUsers()
-      .then(users => {
-        setUserList(users)
+      .then((users) => {
+        setUserList(users);
       })
-      .catch(error => {
-        throw error
+      .catch((error) => {
+        throw error;
+        // something something errors
       });
-    
   }, []);
+
+  useEffect(() => {
+    if (currentUser === null) {
+      setUserPosts([]);
+      setUserTodos([]); // NEW
+      return;
+    }
+
+    getPostsByUser(currentUser.id)
+      .then((posts) => {
+        setUserPosts(posts);
+      })
+      .catch((error) => {
+        throw error;
+        // something something errors
+      });
+
+    // NEW
+    getTodosByUser(currentUser.id)
+      .then((todos) => {
+        setUserTodos(todos);
+      })
+      .catch((error) => {
+        throw error;
+        // something something errors
+      });
+  }, [currentUser]);
 
   return (
     <div id="App">
-      <Header userList={ userList } setCurrentUser = { setCurrentUser } currentUser= { currentUser }/>
-      
+      <Header
+        userList={userList}
+        currentUser={currentUser}
+        setCurrentUser={setCurrentUser}
+      />
+      {currentUser !== null ? ( // MODIFIED
+        <div>
+          <UserPosts userPosts={userPosts} currentUser={currentUser} />
+          <UserTodos userTodos={userTodos} currentUser={currentUser} />
+        </div>
+      ) : null}
     </div>
   );
-}
+};
 
-ReactDOM.render(
-  <App />,
-  document.getElementById('root')
-);
+ReactDOM.render(<App />, document.getElementById("root"));
